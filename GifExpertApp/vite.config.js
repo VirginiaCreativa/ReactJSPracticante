e05ed477ba5e2eb748819e59c12321bbde0cb886
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-11-16 16:20:13 -0500

    soluction dist part 10

--- GifExpertApp/vite.config.js
+++ GifExpertApp/vite.config.js
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "https://virginiacreativa.github.io/ReactJSPracticante/GifExpertApp/",
+  base: "./",
   plugins: [react()],
   server: {
     port: 3000,
